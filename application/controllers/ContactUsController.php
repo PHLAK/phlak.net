62da63d184506da2d4f7242c587a7000f438de83
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2010-02-04 11:48:51 -0700

    Added Blog, Image Gallery and Contact Us pages

---
+++ application/controllers/ContactUsController.php
@@ -0,0 +1,18 @@
+<?php
+
+class ContactUsController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        $this->view->title = 'Contact Us';
+    }
+
+
+}
+
