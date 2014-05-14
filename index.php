d767579d3f6d2a94a11a6725f9d14331f53979b4
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2014-05-14 08:17:44 -0700

    Major simplification of site layout

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>PHLAKNET</title>
+    <link href="/img/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
+
+    <!-- META -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- STYLES / FONTS -->
+    <link href="/css/bootstrap.min.css" media="screen" rel="stylesheet" type="text/css">
+    <link href="/css/style.css" media="screen" rel="stylesheet" type="text/css">
+
+    <!-- SCRIPTS -->
+    <script type="text/javascript" src="//code.jquery.com/jquery-2.1.0.min.js"></script>
+    <script type="text/javascript" src="/js/bootstrap.min.js"></script>
+
+</head>
+
+
+<body>
+
+    <div class="alignment-helper"></div>
+    <img src="/img/PHLAK.png" alt="PHLAK" class="logo">
+
+    <div class="copyright">
+        &copy; <?php echo date('Y'); ?>
+        <a href="https://www.chriskankiewicz.com">Chris Kankiewicz</a>
+    </div>
+
+</body>
+
+</html>
