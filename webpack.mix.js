1dd645acc6d60a5e89aee02753c5b4a55a8afac2
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2019-08-12 23:15:01 -0700

    Installed and configured purgecss

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,7 @@
 let mix = require('laravel-mix');
 let build = require('./tasks/build.js');
 let tailwindcss = require('tailwindcss');
+require('laravel-mix-purgecss');
 
 mix.disableSuccessNotifications();
 mix.setPublicPath('source/assets/build');
@@ -21,3 +22,7 @@ mix.sass('source/_assets/sass/main.scss', 'css').options({
     processCssUrls: false,
     postCss: [tailwindcss('tailwind.config.js')]
 }).version();
+
+mix.purgeCss({
+    folders: ['source']
+});
