013fb868cb089fc964a503216dfbbff901f54120
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2019-11-27 13:39:10 -0700

    Updated copyright link hover color

--- source/index.blade.php
+++ source/index.blade.php
@@ -8,7 +8,7 @@
     <div class="absolute text-gray-700 text-xs bottom-0 right-0 p-4">
         &copy; {{ date('Y') }}
 
-        <a href="https://www.chriskankiewicz.com" class="underline hover:text-pink-700">
+        <a href="https://www.chriskankiewicz.com" class="underline hover:text-gray-600">
             Chris Kankiewicz
         </a>
     </div>
